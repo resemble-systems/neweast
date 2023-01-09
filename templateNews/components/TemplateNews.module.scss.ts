e65272f20292e90f5b902c251aa7a97f3bf96329
ajith <120091187+AjithcRs@users.noreply.github.com>
2023-01-09 18:26:51 +0530

    Add files via upload

---
+++ templateNews/components/TemplateNews.module.scss.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+require("./TemplateNews.module.css");
+const styles = {
+  templateNews: 'templateNews_3d07a18a',
+  teams: 'teams_3d07a18a',
+  welcome: 'welcome_3d07a18a',
+  welcomeImage: 'welcomeImage_3d07a18a',
+  links: 'links_3d07a18a',
+  description: 'description_3d07a18a',
+  scroll: 'scroll_3d07a18a',
+  title: 'title_3d07a18a'
+};
+
+export default styles;
+/* tslint:enable */
